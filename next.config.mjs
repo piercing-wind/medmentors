89f6d14ff6f09904235866d18b9e25f881b9bca1
Sourabh Sharma <sourabhsharma5677@gmail.com>
2024-10-05 05:11:17 +0530

    Initial Commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,10 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-   basePath: '/medmentors', // Replace with your repository name
-   assetPrefix: '/medmentors/', // Replace with your repository name
-   images: {
-     unoptimized: true,
-   },
+    reactStrictMode: true,
+    output: 'export',
+    trailingSlash: true, 
+    assetPrefix: isProduction ? 'https://piercing-wind.github.io/medmentors/' : '',
 };
 
 
